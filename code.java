d4d8deb1d300bf8bc5beed1d8efbc6715af0b678
Katya Pavlenko <kpavlenko@yotpo.com>
2022-09-24 19:55:38 +0300

    update docs

--- code.java
+++ code.java
@@ -1,30 +1,22 @@
+// medium article with full example
+// https://medium.com/@skywall/inject-js-into-androids-webview-8845fb5902b7
+
 // copied from docs https://developer.android.com/develop/ui/views/layout/webapps/webview#java
-// init webview with JS enabled
 
-WebView myWebView=(WebView)findViewById(R.id.webview);
-        WebSettings webSettings=myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webView.addJavascriptInterface(new WebAppInterface(this),"Android");
-
-
+// Sending data to JS
 public void sendToJS() {
-        webView.loadUrl("javascript:(getFromAndroid('Katya speaks from Android'))")
+        stringToSend='Katya speaks from Android'
+        // IMPORTANT this line is critical for API
+        webView.loadUrl("javascript:(getFromAndroid('" + stringToSend + "'))")
 }
 
 // add JS interface
 public class WebAppInterface {
-    Context mContext;
-
-    /**
-     * Instantiate the interface and set the context
-     */
-    WebAppInterface(Context c) {
-        mContext = c;
-    }
 
     /**
      * Show a toast from the web page
      */
+    // IMPORTANT: name of this function is critical for API
     @JavascriptInterface
     public void sendMeEvent(String toast) {
         Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
