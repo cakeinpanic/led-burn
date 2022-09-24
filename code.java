becff96ddb3e2cec06d0fc0d89fae606501f6ae2
Katya Pavlenko <kpavlenko@yotpo.com>
2022-09-24 19:25:14 +0300

    send from android to js

--- code.java
+++ code.java
@@ -1,23 +1,33 @@
 // copied from docs https://developer.android.com/develop/ui/views/layout/webapps/webview#java
 // init webview with JS enabled
 
-WebView myWebView = (WebView) findViewById(R.id.webview);
-WebSettings webSettings = myWebView.getSettings();
-webSettings.setJavaScriptEnabled(true);
+WebView myWebView=(WebView)findViewById(R.id.webview);
+        WebSettings webSettings=myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new WebAppInterface(this),"Android");
 
+
+public void sendToJS() {
+        webView.loadUrl("javascript:(getFromAndroid('Katya speaks from Android'))")
+}
 
 // add JS interface
 public class WebAppInterface {
     Context mContext;
 
-    /** Instantiate the interface and set the context */
+    /**
+     * Instantiate the interface and set the context
+     */
     WebAppInterface(Context c) {
         mContext = c;
     }
 
-    /** Show a toast from the web page */
+    /**
+     * Show a toast from the web page
+     */
     @JavascriptInterface
     public void sendMeEvent(String toast) {
         Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
     }
+
 }
