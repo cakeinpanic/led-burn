eec021edd671e07cff92ba4a6b831c62ea3636e4
Katya Pavlenko <kpavlenko@yotpo.com>
2022-10-01 14:40:04 +0300

    add buzzer signals

--- src/app/stage/music/music.component.ts
+++ src/app/stage/music/music.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { JavaService } from '../../services/java.service';
 
 @Component({
-  selector: 'app-music',
-  templateUrl: './music.component.html',
-  styleUrls: ['./music.component.scss']
+	selector: 'app-music',
+	templateUrl: './music.component.html',
+	styleUrls: ['./music.component.scss']
 })
 export class MusicComponent implements OnInit {
 
-  constructor() { }
+	constructor(private javaService: JavaService) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
 
+	}
+
+	stop() {
+		this.javaService.sendToAndroid('BB=S');
+	}
+
+	play() {
+		this.javaService.sendToAndroid('BB=P');
+	}
+
+	next() {
+		this.javaService.sendToAndroid('BB=N');
+	}
+
+	easter() {
+		this.javaService.sendToAndroid('BB=E');
+	}
 }
